d5412e1dd46643b3f3bc8c1e5a1395505466955c
Asma153 <ayedasma1152@gmail.com>
2024-06-20 12:06:27 +0100

    hello

---
+++ script.js
@@ -0,0 +1,37 @@
+let myname ="asma"
+myname ="hello"
+console.log(myname)
+const num= 52.5
+console.log(num * 6)
+console.log(7==9)
+let x = null
+console.log(x)
+const names =["youssef", "esra", "asma", "nour"]
+for (let i=0; i<= names.length; i++){
+    console.log(names[i])
+    console.log(names[i].length)
+}
+for (let i of names ){
+    console.log(i)
+}
+console.log(names[0])
+console.log(names[2])
+const person = {
+    firstName: "yassmine",
+    lastName: "ayed",
+    school: 1 
+}
+console.log(person)
+console.log(person.lastName)
+const students= [
+    {
+    firstName: "ayoub",
+    lastName: "khalil",
+    school: 3
+},{
+    firstName: "haroun",
+    lastName: "ahmed",
+    school: 5
+} ]
+console.log(students[1].firstName) 
+
